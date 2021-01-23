56ca548a8202281f95d65cef46dc476ad694227f
shardul <shardul.aeer@somaiya.edu>
2021-01-23 15:01:54 +0530

    feat: Get all postcards endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,19 +2,27 @@ import * as functions from 'firebase-functions'
 import * as express from 'express'
 import * as cors from 'cors'
 import * as admin from 'firebase-admin'
-import { NotificationData, Query, UserData } from './types'
+import { NotificationData, CreatePostcardQuery, UserData, GetAllPostcardsQuery } from './types'
 
 admin.initializeApp()
 const db = admin.firestore()
 const fcm = admin.messaging()
-
 const usersCol = db.collection('users')
 
 const app = express()
 app.use(cors({ origin: true }))
 
+async function getUserData(key: string): Promise<UserData | undefined> {
+    const response = await usersCol.doc(key).get()
+    return response.data() as UserData | undefined
+}
+
+/**
+ * Create a new postcard
+ * Send push notification and store data to Firestore
+ */
 app.post('/', async (req, res) => {
-    const query = req.query as Query
+    const query = req.query as CreatePostcardQuery
     console.log('Query params', query)
     const { title, body, key, imageUrl } = query
 
@@ -56,8 +64,20 @@ app.post('/', async (req, res) => {
     }
 })
 
-async function getUserData(key: string): Promise<UserData | undefined> {
-    const response = await usersCol.doc(key).get()
-    return response.data() as UserData | undefined
-}
+/**
+ * Get all postcards for a given user
+ */
+app.get('/', async (req, res) => {
+    const { key } = req.query as GetAllPostcardsQuery
+    if (!key) {
+        return res.status(401).send('Missing key')
+    }
+    const userData = await getUserData(key)
+    if (!userData) {
+        return res.status(401).send('Invalid key')
+    }
+
+    return res.status(200).send(userData.postcards)
+})
+
 export const api = functions.region('asia-south1').https.onRequest(app)
--- functions/src/types.d.ts
+++ functions/src/types.d.ts
@@ -1,16 +1,30 @@
-interface Query {
+import * as admin from 'firebase-admin'
+
+interface CreatePostcardQuery {
     title?: string, body?: string, imageUrl?: string, key?: string
 }
 
 interface NotificationData {
     title: string, body: string, imageUrl?: string
 }
+
 interface Postcard extends NotificationData {
-    time: Date
+    time: admin.firestore.Timestamp
 }
+
 interface UserData {
     fcmToken: string,
     postcards?: Postcard[]
 }
 
-export { Query, NotificationData, Postcard, UserData }
+interface GetAllPostcardsQuery {
+    key?: string
+}
+
+export {
+    CreatePostcardQuery,
+    NotificationData,
+    Postcard,
+    UserData,
+    GetAllPostcardsQuery
+}
